b19cb4881c12dc6ea53f2e0d3d67acfe1aba5375
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-02-17 22:25:46 +0000

    Wrote setBarHeight() function to set the height of each bar based on the height of the highest value in the data array

--- app.js
+++ app.js
@@ -5,29 +5,37 @@ const barsDivArray = document.getElementsByClassName('bars');
 
 //Informatio need to draw a bar
 const element = barsDivArray;
-let data = [1, 2, 3, 4]; //make 4 bars
+let data = [1, 2, 3, 4, 5]; //make 4 bars
 let options = {
   barWidth: '50px',
-  barHeight: '50%',
+  barHeight: '',
   barColor: 'red'
 };
+//------------------------------
 
 //-----------Main functions-----------//
 function drawBarChart(data, options, element) {
   //determine highest, determine lowest --> determine heights
 
+
   //determine number of bars --> determine spacing per bar %
   const barSpacing = (80 / data.length);
 
   //for each element in array, create a new bar and draw each
-  for (let i = 1 ; i <= data.length; i++) {
+  for (let i = 0 ; i < data.length; i++) {
     let newDiv = document.createElement('div');
     canvas.appendChild(newDiv);
     newDiv.style.setProperty('--width', options.barWidth);
-    newDiv.style.setProperty('--left', barSpacing * i);
+    newDiv.style.setProperty('--left', barSpacing * (i + 1));
     newDiv.style.setProperty('--color', options.barColor);
-    newDiv.style.setProperty('--height', options.barHeight);
+    newDiv.style.setProperty('--height', setBarHeight(data[i]) - 5);
   }
+}
+
+function setBarHeight(num) {
+  const maxHeight = arrayMax(data);
+  let heightRatio = (num / maxHeight);
+  return heightRatio * 100;
 }
 
 
@@ -53,5 +61,6 @@ function arrayMin(arr) {
 }
 
 //---console logs
+console.log(setBarHeight(2));
 
 drawBarChart(data, options, element);
